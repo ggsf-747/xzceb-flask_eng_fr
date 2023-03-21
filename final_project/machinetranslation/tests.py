ffac04ae21b923e3fbdd1522ba5847ec452a7edf
ggsf-747 <ggsfermanis@gmail.com>
2023-03-20 20:41:35 -0400

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,16 +6,16 @@ class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french("Hello"), "Bonjour")
         self.assertEqual(english_to_french("Yes"), "Oui")
-        self.assertEqual(english_to_french("Boy"), "Garçon")
-        self.assertEqual(english_to_french(""), "error")
+        self.assertNotEqual(english_to_french("Table"), "Garçon")
+        self.assertNotEqual(english_to_french(""), "error")
 
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
         self.assertEqual(french_to_english("Oui"), "Yes")
-        self.assertEqual(french_to_english("Garçon"), "Boy")
-        self.assertEqual(french_to_english(""), "error")
+        self.assertNotEqual(french_to_english("Garçon"), "Table")
+        self.assertNotEqual(french_to_english(""), "error")
 
 
 if __name__ == '__main__':
