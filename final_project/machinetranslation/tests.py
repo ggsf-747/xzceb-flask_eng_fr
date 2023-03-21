78d42db0aba97558faad9decdaa18c8c71d39c59
ggsf-747 <ggsfermanis@gmail.com>
2023-03-20 21:05:56 -0400

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,16 +6,20 @@ class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french("Hello"), "Bonjour")
         self.assertEqual(english_to_french("Yes"), "Oui")
+    def test2(self):
         self.assertNotEqual(english_to_french("Table"), "Garçon")
-        self.assertNotEqual(english_to_french(""), "error")
+    def test3(self):
+        self.assertNotEqual(english_to_french(" "), "null")
 
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
         self.assertEqual(french_to_english("Oui"), "Yes")
+    def test2(self):
         self.assertNotEqual(french_to_english("Garçon"), "Table")
-        self.assertNotEqual(french_to_english(""), "error")
+    def test3(self):
+        self.assertNotEqual(french_to_english(" "), "null")
 
 
 if __name__ == '__main__':
--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -6,16 +6,20 @@ class TestEnglishToFrench(unittest.TestCase):
     def test1(self):
         self.assertEqual(english_to_french("Hello"), "Bonjour")
         self.assertEqual(english_to_french("Yes"), "Oui")
-        self.assertEqual(english_to_french("Boy"), "Garçon")
-        self.assertEqual(english_to_french(""), "error")
+    def test2(self):
+        self.assertNotEqual(english_to_french("Table"), "Garçon")
+    def test3(self):
+        self.assertNotEqual(english_to_french(" "), "null")
 
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
         self.assertEqual(french_to_english("Oui"), "Yes")
-        self.assertEqual(french_to_english("Garçon"), "Boy")
-        self.assertEqual(french_to_english(""), "error")
+    def test2(self):
+        self.assertNotEqual(french_to_english("Garçon"), "Table")
+    def test3(self):
+        self.assertNotEqual(french_to_english(" "), "null")
 
 
 if __name__ == '__main__':
